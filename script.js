84cf1200baf00f621ae7ccefe1551db811d9297a
GALU <GaluStudios@gmail.com>
2026-06-06 14:50:49 -0500

    GALU v1.2

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-console.log("GALU iniciado");
-const cards=document.querySelectorAll(".card");
-
-cards.forEach(card=>{
-
-card.addEventListener("mousemove",()=>{
-
-card.style.transform="translateY(-8px) scale(1.02)";
-
-});
-
-card.addEventListener("mouseleave",()=>{
-
-card.style.transform="translateY(0) scale(1)";
-
-});
-
+// EXPLORE scroll
+document.querySelector(".explore-btn").addEventListener("click", () => {
+  document.querySelector(".discover").scrollIntoView({
+    behavior: "smooth"
+  });
 });
