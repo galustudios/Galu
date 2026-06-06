434aae3098d61b9ab0387f585b228e8125c96c35
GALU <GaluStudios@gmail.com>
2026-06-06 14:36:41 -0500

    Actualizar diseño de la página

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("GALU iniciado");
+const cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+card.addEventListener("mousemove",()=>{
+
+card.style.transform="translateY(-8px) scale(1.02)";
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="translateY(0) scale(1)";
+
+});
+
+});
