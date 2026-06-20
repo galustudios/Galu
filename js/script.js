30739ff076bb93781a122f62dd1d50d13f6c1b5f
GALU <GaluStudios@gmail.com>
2026-06-20 15:32:56 -0500

    fix: rutas absolutas para navbar y footer

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  fetch("components/navbar.html")
+  fetch("/components/navbar.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   });
-  fetch("components/footer.html")
+  fetch("/components/footer.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
